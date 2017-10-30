979564221ff4c9cc8b8f2003f7e29bd320771b39
Sebastian <s.tschiedel@gmail.com>
2017-10-30 13:16:24 +0100

    added documentation

--- nucleotide-count/nucleotide.go
+++ nucleotide-count/nucleotide.go
@@ -6,9 +6,12 @@ import (
 
 const testVersion = 2
 
+// DNA is the model for an DNA string
 type DNA string
 type nucleotide byte
 
+// Count returns the number ob nucleotides of an DNA string or an Error
+// if the string contains invalid characters
 func (d DNA) Count(b byte) (int, error) {
 	n := nucleotide(b)
 	if !n.isValid() {
@@ -23,6 +26,7 @@ func (d DNA) Count(b byte) (int, error) {
 	return sum, nil
 }
 
+// isValid checks if a nucleotide is a valid DNA character
 func (n nucleotide) isValid() bool {
 	switch n {
 	case 'A', 'C', 'G', 'T':
@@ -33,6 +37,7 @@ func (n nucleotide) isValid() bool {
 
 }
 
+// isValid checks if a string is a valid DNA string
 func (d DNA) isValid() bool {
 	for _, v := range d {
 		if !nucleotide(v).isValid() {
@@ -42,8 +47,10 @@ func (d DNA) isValid() bool {
 	return true
 }
 
+// Histogram is a map that counts the number of character in an DNA string
 type Histogram map[rune]int
 
+// Counts parts of an dna string and returns an error if an dna is not valid
 func (d DNA) Counts() (h Histogram, err error) {
 	if !d.isValid() {
 		return nil, errors.New("DNA is not valid")
