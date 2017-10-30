1277d7539ce1852ce742bcdb2f26ebb875d97b86
Sebastian <s.tschiedel@gmail.com>
2017-10-30 17:25:20 +0100

    added and solved protein translation

---
+++ protein-translation/protein_translation.go
@@ -0,0 +1,40 @@
+package protein
+
+const testVersion = 1
+
+// FromCondon translates one RNA sequence to Proteins
+func FromCodon(in string) string {
+	var m map[string]string = map[string]string{
+		"AUG": "Methionine",
+		"UUU": "Phenylalanine",
+		"UUC": "Phenylalanine",
+		"UUA": "Leucine",
+		"UUG": "Leucine",
+		"UCU": "Serine",
+		"UCC": "Serine",
+		"UCA": "Serine",
+		"UCG": "Serine",
+		"UAU": "Tyrosine",
+		"UAC": "Tyrosine",
+		"UGU": "Cysteine",
+		"UGC": "Cysteine",
+		"UGG": "Tryptophan",
+		"UAA": "STOP",
+		"UAG": "STOP",
+		"UGA": "STOP",
+	}
+	return m[in]
+}
+
+// FromRNA translates polypetides to a sequence of proteins
+// stops if a STOP codon is read
+func FromRNA(in string) (out []string) {
+	for i := 0; i < len(in); i += 3 {
+		cur := FromCodon(in[i : i+3])
+		if cur == "STOP" {
+			break
+		}
+		out = append(out, cur)
+	}
+	return out
+}
