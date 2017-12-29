74b241d6eac9c201a33c6b38fde303467d814e2a
Sebastian <s.tschiedel@gmail.com>
2017-12-29 03:15:15 +0100

    solved bracket-push

---
+++ bracket-push/bracket_push.go
@@ -0,0 +1,22 @@
+package brackets
+
+func Bracket(in string) (bool, error) {
+	opener := map[rune]rune{
+		']': '[',
+		')': '(',
+		'}': '{',
+	}
+	stack := make([]rune, 0)
+	for _, c := range in {
+		switch c {
+		case '[', '(', '{':
+			stack = append(stack, c)
+		case ']', ')', '}':
+			if len(stack) == 0 || stack[len(stack)-1] != opener[c] {
+				return false, nil
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	return len(stack) == 0, nil
+}
