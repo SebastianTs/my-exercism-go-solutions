481645c7ceebd350840e78d0b60291afb65a8931
Sebastian <s.tschiedel@gmail.com>
2017-12-31 01:41:49 +0100

    solved custom_set

---
+++ custom-set/custom_set.go
@@ -0,0 +1,126 @@
+package stringset
+
+// Set represents a set of strings
+type Set struct {
+	data map[string]struct{}
+}
+
+// New creates an empty set
+func New() Set {
+	return Set{data: make(map[string]struct{})}
+}
+
+// NewFromSlice creates an Set from a given list of strings
+func NewFromSlice(list []string) Set {
+	set := New()
+	for _, element := range list {
+		set.data[element] = struct{}{}
+	}
+	return set
+}
+
+// String creates a string from a set
+func (s Set) String() string {
+	out := "{"
+	for v := range s.data {
+		out += "\"" + v + "\", "
+	}
+	// remove last ", "
+	if len(out) != 1 {
+		out = out[:len(out)-2]
+	}
+	return out + "}"
+}
+
+// IsEmpty is true if an Set is Empty
+func (s Set) IsEmpty() bool {
+	return len(s.data) == 0
+}
+
+// Has checks if the given element is in the set
+func (s Set) Has(elem string) bool {
+	_, ok := s.data[elem]
+	return ok
+}
+
+// Subset is true when s1 is a subset of s2
+func Subset(s1, s2 Set) bool {
+	if s1.IsEmpty() {
+		return true
+	}
+	if s2.IsEmpty() {
+		return false
+	}
+	for k := range s1.data {
+		if !s2.Has(k) {
+			return false
+		}
+	}
+	return true
+}
+
+// Disjoint is true when the two given sets have no common element
+func Disjoint(s1, s2 Set) bool {
+	if (s1.IsEmpty() && !s2.IsEmpty()) ||
+		(!s1.IsEmpty() && s2.IsEmpty()) {
+		return true
+	}
+	for k := range s2.data {
+		if s1.Has(k) {
+			return false
+		}
+	}
+	return true
+}
+
+// Equal is true when the given sets have (only) the same elements
+func Equal(s1, s2 Set) bool {
+	if len(s1.data) != len(s2.data) {
+		return false
+	}
+	for k := range s1.data {
+		if !s2.Has(k) {
+			return false
+		}
+	}
+	return true
+}
+
+// Add adds a string to the set
+func (s *Set) Add(elem string) {
+	s.data[elem] = struct{}{}
+}
+
+// Intersection returns the intersection of two sets
+func Intersection(s1, s2 Set) Set {
+	intersect := New()
+	for k := range s1.data {
+		if s2.Has(k) {
+			intersect.Add(k)
+		}
+	}
+	return intersect
+}
+
+// Difference returns the difference of s1 and s2
+func Difference(s1, s2 Set) Set {
+	difference := New()
+	if len(s1.data) == 0 {
+		return difference
+	}
+	for k := range s1.data {
+		if !s2.Has(k) {
+			difference.Add(k)
+		}
+	}
+	return difference
+}
+
+// Union returns the union of two sets
+func Union(s1, s2 Set) Set {
+	union := s1
+	for k := range s2.data {
+		union.Add(k)
+	}
+	return union
+}
