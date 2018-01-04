ca2f647085833e134462d69fdbab764eb89c47cc
Sebastian <s.tschiedel@gmail.com>
2018-01-04 10:44:21 +0100

    solved atbash-cipher

---
+++ atbash-cipher/atbash_cipher.go
@@ -0,0 +1,30 @@
+package atbash
+
+import "strings"
+
+const groupSize = 5
+
+func Atbash(str string) (result string) {
+	str = strings.ToLower(str)
+	offset := 'z' - 'a'
+	i := 0
+	for _, c := range str {
+		switch {
+		case c >= 'a' && c < 'z':
+			result += string('z' - ((c + 3) % offset))
+			i++
+		case c == 'z':
+			result += string('a')
+			i++
+		case c >= '0' && c <= '9':
+			result += string(c)
+			i++
+		}
+		if i == groupSize {
+			result += " "
+			i = 0
+		}
+	}
+	result = strings.TrimSpace(result)
+	return
+}
