08746441a1468733a5a898380b442109ef32cef8
Sebastian <s.tschiedel@gmail.com>
2018-01-07 21:35:22 +0100

    removed unneccesary breaks in switch statement

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -13,31 +13,22 @@ func ToRomanNumeral(n int) (out string, err error) {
 		switch n % 10 {
 		case 1:
 			out = rn[i] + out
-			break
 		case 2:
 			out = rn[i] + rn[i] + out
-			break
 		case 3:
 			out = rn[i] + rn[i] + rn[i] + out
-			break
 		case 4:
 			out = rn[i] + rn[i+1] + out
-			break
 		case 5:
 			out = rn[i+1] + out
-			break
 		case 6:
 			out = rn[i+1] + rn[i] + out
-			break
 		case 7:
 			out = rn[i+1] + rn[i] + rn[i] + out
-			break
 		case 8:
 			out = rn[i+1] + rn[i] + rn[i] + rn[i] + out
-			break
 		case 9:
 			out = rn[i] + rn[i+2] + out
-			break
 		}
 		n = n / 10
 		i += 2
