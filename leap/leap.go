029f8590b6ab4a2e09034667884ea0d4d015a33c
Sebastian <s.tschiedel@gmail.com>
2018-01-15 23:43:57 +0100

    simplified return statement

--- leap/leap.go
+++ leap/leap.go
@@ -4,8 +4,5 @@ const testVersion = 3
 
 // IsLeapYear tests if a given int value represents a Leap Year
 func IsLeapYear(year int) bool {
-	if year%4 == 0 && (year%100 != 0 || year%400 == 0) {
-		return true
-	}
-	return false
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
